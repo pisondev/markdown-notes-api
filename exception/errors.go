2c040e8ff46073bd53d37e013190c11786eb7bce
Pison Golda <pison.studyroom@gmail.com>
2025-08-13 22:48:57 +0700

    feat: add error unauthorized login var

--- exception/errors.go
+++ exception/errors.go
@@ -3,5 +3,6 @@ package exception
 import "errors"
 
 var (
-	ErrConflictUser = errors.New("username already exist")
+	ErrConflictUser      = errors.New("username already exist")
+	ErrUnauthorizedLogin = errors.New("invalid username or password")
 )
