62a69f8d1e82e36da6e7d8a7d40b754e413181c9
Pison Golda <pison.studyroom@gmail.com>
2025-08-13 22:50:25 +0700

    fix: fix validationErr logic & add 401 unauthorized login

--- exception/error_handler.go
+++ exception/error_handler.go
@@ -11,11 +11,17 @@ import (
 func ErrorHandler(c *fiber.Ctx, err error) error {
 	code := fiber.StatusInternalServerError
 	status := "INTERNAL SERVER ERROR"
-
-	validationErr := err.(validator.ValidationErrors)
+	//400
+	var validationErr validator.ValidationErrors
 	if errors.As(err, &validationErr) {
 		code = fiber.ErrBadRequest.Code
 		status = "BAD REQUEST"
+	}
+
+	//401
+	if errors.Is(err, ErrUnauthorizedLogin) {
+		code = fiber.StatusUnauthorized
+		status = "UNAUTHORIZED"
 	}
 
 	//409
