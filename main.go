2021b68dc4388724c050c9f2a4f4ac66229bb76b
Pison Golda <pison.studyroom@gmail.com>
2025-08-13 21:03:10 +0700

    feat: inject dependencies, register POST /register & POST /login route

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"os"
+	"pisondev/markdown-notes-api/app"
+	"pisondev/markdown-notes-api/controller"
+	"pisondev/markdown-notes-api/repository"
+	"pisondev/markdown-notes-api/service"
 
+	"github.com/go-playground/validator/v10"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
@@ -16,10 +22,20 @@ func main() {
 	}
 	serverPort := os.Getenv("SERVER_PORT")
 
-	app := fiber.New()
+	db := app.NewDB()
+	validate := validator.New()
+
+	userRepository := repository.NewUserRepository(log)
+	userService := service.NewUserService(userRepository, db, validate, log)
+	userController := controller.NewUserController(userService, log)
+
+	server := fiber.New()
 	log.Infof("server starting on port %s...", serverPort)
 
-	err = app.Listen(serverPort)
+	server.Post("/register", userController.Register)
+	server.Post("/login", userController.Login)
+
+	err = server.Listen(serverPort)
 	if err != nil {
 		log.Fatalf("error starting server: %v", err)
 	}
