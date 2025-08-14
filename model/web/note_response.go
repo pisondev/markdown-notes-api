a7a4046c886598773cedccd86d977d1f759ef66f
Pison Golda <pison.studyroom@gmail.com>
2025-08-14 15:33:46 +0700

    feat: add PaginatedNoteResponse struct model

--- model/web/note_response.go
+++ model/web/note_response.go
@@ -7,3 +7,15 @@ type NoteResponse struct {
 	OriginalFilename string    `json:"originalFileName"`
 	CreatedAt        time.Time `json:"createdAt"`
 }
+
+type PaginatedNoteResponse struct {
+	Data       []NoteResponse `json:"data"`
+	Pagination Pagination     `json:"pagination"`
+}
+
+type Pagination struct {
+	CurrentPage int `json:"currentPage"`
+	Limit       int `json:"limit"`
+	TotalItems  int `json:"totalItems"`
+	TotalPages  int `json:"totalPages"`
+}
